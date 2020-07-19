b68a9fef75aae53b4881dc1600fc97e706c4aa5c
SharovaOV <olyaart851@mail.ru>
2020-07-19 13:13:04 +0300

    models1

--- ReitingM/my_func.py
+++ ReitingM/my_func.py
@@ -6,9 +6,9 @@ class model_learn:
     rating=0
     positive=False
     def __init__(self, text_val):
-        self.model_wordbag= joblib.load('models/wordbag.pkl')
-        self.model_rating=joblib.load('models/ratingmodel.pkl')
-        self.model_positiv = joblib.load('models/positivmodel.pkl')
+        self.model_wordbag= joblib.load('../models/wordbag.pkl')
+        self.model_rating=joblib.load('../models/ratingmodel.pkl')
+        self.model_positiv = joblib.load('../models/positivmodel.pkl')
         text_val=self.model_wordbag.transform([text_val])
         self.rating = np.argmax(self.model_rating.predict(text_val))+1
         self.positive = round(self.model_positiv.predict(text_val)[0][0])
