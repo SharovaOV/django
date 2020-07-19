d00b98e17ec50cf77664d9ab7e506275a2861d8a
SharovaOV <olyaart851@mail.ru>
2020-07-19 12:51:39 +0300

    style 4

--- Django/settings.py
+++ Django/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'ReitingM')
+STATIC_URL = '/ReitingM/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'ReitingM/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
